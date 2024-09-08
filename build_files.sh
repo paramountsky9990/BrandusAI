c78db53ff29b6fb744b463081315bf5904f1b791
paramountsky9990 <137667254+paramountsky9990@users.noreply.github.com>
2024-09-08 12:39:50 +0900

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,27 +1,27 @@
 # echo "build loading.........."
-# pip3 install -r requirements.txt
-
-# echo "staticfiles build....."
-# python3 manage.py collectstatic --noinput
-
-#!/bin/bash
-
-echo "Building loading.........."
 pip3 install -r requirements.txt
 
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to install dependencies."
-    exit 1
-fi
-
-echo "Static files build....."
+# echo "staticfiles build....."
 python3 manage.py collectstatic --noinput
 
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to collect static files."
-    exit 1
-fi
+# #!/bin/bash
 
-echo "Build completed successfully."
+# echo "Building loading.........."
+# pip3 install -r requirements.txt
+
+# if [ $? -ne 0 ]; then
+#     echo "Error: Failed to install dependencies."
+#     exit 1
+# fi
+
+# echo "Static files build....."
+# python3 manage.py collectstatic --noinput
+
+# if [ $? -ne 0 ]; then
+#     echo "Error: Failed to collect static files."
+#     exit 1
+# fi
+
+# echo "Build completed successfully."
 
 
